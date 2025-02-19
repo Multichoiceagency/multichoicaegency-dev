978eaf12c4fa9ebc9319bed2832cc2695d8dbd80
Multichoiceagency <139119345+Multichoiceagency@users.noreply.github.com>
2025-02-19 01:08:43 +0100

    Update
    Suppreshydrationwarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={hankenGrotesk.className}>
         <ThemeProvider
           attribute="class"
