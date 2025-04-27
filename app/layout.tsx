de51af53d0f4bd6a88dba746096440b5101036c6
Multichoiceagency <139119345+Multichoiceagency@users.noreply.github.com>
2025-04-27 10:16:53 +0200

    UPDATE

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import ScrollToTop from "@/components/ScrollToTop"
 import FloatingSocialIcons from "@/components/FloatingSocialicons"
 import SmoothScroll from "@/components/Smooth-scroll"
 import FooterSection from "@/components/footer-section"
+import Script from "next/script"
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -32,6 +33,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Header />
           <SmoothScroll>
             <main>{children}</main>
+            <Script
+          src="https://api.lindy.ai/api/lindyEmbed/lindyEmbed.js?a=1215665e-af0b-4ff6-ba9f-8aa4946a1002"
+          strategy="afterInteractive"
+          crossOrigin="use-credentials"
+        />
             <FooterSection />
             </SmoothScroll>
         </ThemeProvider>
