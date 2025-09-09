df577ec60c26ac2b8eaf7fa0f110684c0941b906
Multichoiceagency <139119345+Multichoiceagency@users.noreply.github.com>
2025-09-09 12:18:07 +0200

    update

--- app/cases/page.tsx
+++ app/cases/page.tsx
@@ -1,5 +1,7 @@
 import { fetchCases } from '@/lib/api';
 import CasesPageClient from '@/components/cases';
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export async function generateStaticParams() {
   const cases = await fetchCases();
