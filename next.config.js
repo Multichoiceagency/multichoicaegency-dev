5049b51c500f111a53d1f8fe0d02c1ff2f147597
Multichoiceagency <139119345+Multichoiceagency@users.noreply.github.com>
2025-09-09 12:04:30 +0200

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // SW uit in development
+  runtimeCaching: require("next-pwa/cache"),
 })
 
 /** @type {import('next').NextConfig} */
